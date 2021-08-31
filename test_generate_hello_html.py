30fec015bd3c5c31a27b2e53d1611326d8a6a887
Nopparat Apichatsiritham <nop.ee38@gmail.com>
2021-08-31 17:16:30 +0700

    change

--- test_generate_hello_html.py
+++ test_generate_hello_html.py
@@ -4,5 +4,6 @@ from generate_hello_html import generate_hello_html
 def test_hello_gihub_actions():
     assert generate_hello_html("") == "<span>Hello Github Actions!</span>"
 
+    
 def test_hello_someone():
     assert generate_hello_html("Athibet") == "<span>Hello Athibet!</span>"
